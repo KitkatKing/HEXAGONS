5d15ce14a7dce30dea4180bd9ca101195cdc60bc
KatKatKatKatKat <74123977+KitkatKing@users.noreply.github.com>
2023-02-14 23:40:49 -0500

    Dynamic chunk Renderer as well as seperated mesh generation steps

--- Assets/Script/Chunk.cs
+++ Assets/Script/Chunk.cs
@@ -26,7 +26,13 @@ public class Chunk : MonoBehaviour
 
     public List<int> tris = new List<int>();
 
+    public bool isStartCreateGen = false;
 
+    public bool isVerticesGen = false;
+
+    public bool isTrisGen = false;
+
+    public bool isMeshGen = false;
 
 
     public Chunk(Vector3Int chunkPosition, MeshFilter mesh, Material material)
@@ -44,23 +50,37 @@ public class Chunk : MonoBehaviour
 
         this.material = material;
 
-
     }
 
-    public void DoShit()
+    public void VoxelCreationCall()
     {
-        if(chunkPosition.y < 3)
+        if (chunkPosition.y < 3)
         {
             CreateAllHex();
         }
 
         //CreateOneHex(new Vector3Int(16,16,16));
+    }
+
+    public void VerticesGenerationCall()
+    {
         generate_vertices();
+    }
+
+    public void UvTriCreationCall()
+    {
         generate_uvs();
         generate_tris();
-        MakeMesh();
-
     }
+
+    public void MeshColliderCall()
+    {
+        MakeMesh();
+    }
+
+
+
+
 
 
     public void CreateAllHex()
@@ -101,6 +121,8 @@ public class Chunk : MonoBehaviour
 
     private void add_vertex(Vector3 v) {
       vertices.Add(v); }
+
+
     private void generate_vertices() {
       Vector3 x_a = new Vector3(0.75f, 0.75f, 0.88f);
 
@@ -117,6 +139,8 @@ public class Chunk : MonoBehaviour
 
     private void add_uv(Vector2 a) {
       uv.Add(a); }
+
+
     private void generate_uvs() {
       for (int x = 0; x < CHUNK_SIZE.x; x++) {
         for (int y = 0; y < CHUNK_SIZE.y; y++) {
@@ -135,6 +159,7 @@ public class Chunk : MonoBehaviour
 
       for (int x = 0; x < CHUNK_SIZE.x; x++) {
         for (int y = 0; y < CHUNK_SIZE.y; y++) {
+
           for (int z = 0; z < CHUNK_SIZE.z; z++) {
             if (chunkData[x, y, z] != null) {
               if (does_hex_y_plus_exist(x, y, z)) {
@@ -195,35 +220,50 @@ public class Chunk : MonoBehaviour
     // reason for using 3 integers rather than a vector is nothing
     // specific to vectors is in this function or returned by it
     private bool does_hex_y_plus_exist(int x, int y, int z) {
-      // double bars are necessary to avoid an IndexOutOfBounds exception by checking the left side first
-      if ((y + 1 >= CHUNK_SIZE.y) || (chunkData[x, y + 1, z] == null)) {
+
+       // if ((y + 1 >= CHUNK_SIZE.y)) { return false; }
+
+        // double bars are necessary to avoid an IndexOutOfBounds exception by checking the left side first
+        if ((y + 1 >= CHUNK_SIZE.y) || (chunkData[x, y + 1, z] == null)) {
         return true;
       } else {
         return false;
       }
     }
     private bool does_hex_y_minus_exist(int x, int y, int z) {
-      // further reduction of the previous because:
-      // "if condition { return true; } else { return false; }" == "return condition"
 
-      // note the double bars "||" are necessary because it evaluates the
-      // right condition only if the left condition evaluates to false,
-      // compared to single bar "|" which checks both.
-      return ((y <= 0) ||
+        if ((y <= 0)) { return false; }
+
+        // further reduction of the previous because:
+        // "if condition { return true; } else { return false; }" == "return condition"
+
+        // note the double bars "||" are necessary because it evaluates the
+        // right condition only if the left condition evaluates to false,
+        // compared to single bar "|" which checks both.
+        return ((y <= 0) ||
               (chunkData[x, y - 1, z] == null));
     }
 
     private bool does_hex_z_plus_exist(int x, int y, int z) {
+
+        if((z + 1 >= CHUNK_SIZE.z)) { return false; }
+
       return ((z + 1 >= CHUNK_SIZE.z) ||
               (chunkData[x, y, z + 1] == null));
     }
     private bool does_hex_z_minus_exist(int x, int y, int z) {
+
+           if((z <= 0)) { return false; }
+
       return ((z <= 0) ||
               (chunkData[x, y, z - 1] == null));
     }
 
     private bool does_hex_x_plus_z_plus_exist(int x, int y, int z) {
-      return (((x + 1 >= CHUNK_SIZE.x) ||
+
+        if ((x + 1 >= CHUNK_SIZE.x)) { return false; }
+
+        return (((x + 1 >= CHUNK_SIZE.x) ||
               // this check is necessary because we add to the value
               // of z, depending on which x hexagon layer it is in,
               // in the generate_tris function.
@@ -231,17 +271,26 @@ public class Chunk : MonoBehaviour
               (chunkData[x + 1, y, z] == null));
     }
     private bool does_hex_x_minus_z_plus_exist(int x, int y, int z) {
-      return (((x <= 0) ||
+
+        if ((x <= 0) || (z >= CHUNK_SIZE.z)) { return false; }
+
+        return (((x <= 0) ||
                (z >= CHUNK_SIZE.z)) ||
               (chunkData[x - 1, y, z] == null));
     }
     private bool does_hex_x_plus_z_minus_exist(int x, int y, int z) {
-      return (((x + 1 >= CHUNK_SIZE.x) ||
+
+        if ((x + 1 >= CHUNK_SIZE.x) || (z <= 0)) { return false; }
+
+        return (((x + 1 >= CHUNK_SIZE.x) ||
                (z <= 0)) ||
               (chunkData[x + 1, y, z] == null));
     }
     private bool does_hex_x_minus_z_minus_exist(int x, int y, int z) {
-      return (((x <= 0) ||
+
+        if ((x <= 0) || (z < 0)) { return false; }
+
+        return (((x <= 0) ||
                (z < 0)) ||
               (chunkData[x - 1, y, z] == null));
     }
--- Assets/Script/World.cs
+++ Assets/Script/World.cs
@@ -12,7 +12,7 @@ public class World : MonoBehaviour
 
     public Material material;
 
-    public int renderSize;
+    private Vector3Int previous_player_chunk_pos;
 
     public GameObject ok;
 
@@ -28,14 +28,17 @@ public class World : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-        if(Input.GetKeyUp(KeyCode.Space))
+
+        UpdateChunksAroundPlayer(PlayerToChunk(ok.transform.position));
+
+
+        if (Input.GetKeyUp(KeyCode.Space))
         {
 
-            PlayerLoad(PlayerToChunk(ok.transform.position));
+          //  PlayerLoad(PlayerToChunk(ok.transform.position), 10, 5, 3, 2);
            
         }
-        if (Input.GetKeyDown(KeyCode.A)) {
+        if (Input.GetKeyDown(KeyCode.LeftControl)) {
           GameObject.Find("Player").transform.position = new Vector3(22, 122, 28);
         }
 
@@ -61,24 +64,6 @@ public class World : MonoBehaviour
     }
 
 
-
-
-
-
-    public int ClampWorldX(int number)
-    {
-
-        return Mathf.Clamp(number, 0, WORLD_SIZE.x - 1);
-
-    }
-
-    public int ClampWorldY(int number)
-    {
-
-        return Mathf.Clamp(number, 0, WORLD_SIZE.y - 1);
-
-    }
-
     public Vector3Int PlayerToChunk(Vector3 PlayerPos)
     {
 
@@ -87,35 +72,118 @@ public class World : MonoBehaviour
     }
 
 
-
-    public void PlayerLoad(Vector3Int chunkPosition)
+    private void UpdateChunksAroundPlayer(Vector3Int ChunkPos)
     {
-
-        for (int x = ClampWorldX(chunkPosition.x - renderSize); x < ClampWorldX(chunkPosition.x + renderSize); x++)
+        if (!ChunkPos.Equals(previous_player_chunk_pos))
         {
+<<<<<<< Updated upstream
             for (int y = ClampWorldY(chunkPosition.y - renderSize / 2); y < ClampWorldY(chunkPosition.y + renderSize / 2); y++)
-            {
-                for (int z = ClampWorldX(chunkPosition.z - renderSize); z < ClampWorldX(chunkPosition.x + renderSize); z++)
-                {
-                    if(world[x, y, z].vertices.Count == 0)
-                    {
+=======
+            previous_player_chunk_pos = ChunkPos;
+            PlayerLoad(ChunkPos, 5, 5, 5, 5);
+        }
 
+    }
+
+
+        public void PlayerLoad(Vector3Int chunkPosition, int totalSize, int verticesSize, int TriSize, int ColliderSize)
+        {
+        for (int x = chunkPosition.x - totalSize; x < chunkPosition.x + totalSize; x++)
+        {
+            for (int y = chunkPosition.y - totalSize; y < chunkPosition.y + totalSize; y++)
+>>>>>>> Stashed changes
+            {
+                for (int z = chunkPosition.z - totalSize; z < chunkPosition.z + totalSize; z++)
+                {
+                    if (x >= 0 && y >= 0 && z >= 0 && x < WORLD_SIZE.x && y < WORLD_SIZE.y && z < WORLD_SIZE.x)
+                    {
+                        if (world[x, y, z].isStartCreateGen == false)
+                        {
+
+<<<<<<< Updated upstream
                         Debug.Log(chunkPosition);
                         
                         world[x, y, z].DoShit();
+=======
+                            world[x, y, z].VoxelCreationCall();
+>>>>>>> Stashed changes
 
+                            world[x, y, z].isStartCreateGen = true;
+
+                        }
                     }
-                    else
-                    {
-                        Debug.Log("Nah");
-                    }
-
-
                 }
             }
         }
 
+        for (int x = chunkPosition.x - verticesSize; x < chunkPosition.x + verticesSize; x++)
+        {
+            for (int y = chunkPosition.y - verticesSize; y < chunkPosition.y + verticesSize; y++)
+            {
+                for (int z = chunkPosition.z - verticesSize; z < chunkPosition.z + verticesSize; z++)
+                {
+                    if (x >= 0 && y >= 0 && z >= 0)
+                    {
+                        if (world[x, y, z].isVerticesGen == false)
+                        {
+
+                            world[x, y, z].VerticesGenerationCall();
+
+                            world[x, y, z].isVerticesGen = true;
+
+                        }
+                    }
+                }
+            }
+        }
+
+        for (int x = chunkPosition.x - TriSize; x < chunkPosition.x + TriSize; x++)
+        {
+            for (int y = chunkPosition.y - TriSize; y < chunkPosition.y + TriSize; y++)
+            {
+                for (int z = chunkPosition.z - TriSize; z < chunkPosition.z + TriSize; z++)
+                {
+                    if (x >= 0 && y >= 0 && z >= 0)
+                    {
+                        if (world[x, y, z].isTrisGen == false)
+                        {
+
+                            world[x, y, z].UvTriCreationCall();
+
+                            world[x, y, z].isTrisGen = true;
+
+                        }
+                    }
+                }
+            }
+        }
+
+        for (int x = chunkPosition.x - ColliderSize; x < chunkPosition.x + ColliderSize; x++)
+        {
+            for (int y = chunkPosition.y - ColliderSize; y < chunkPosition.y + ColliderSize; y++)
+            {
+                for (int z = chunkPosition.z - ColliderSize; z < chunkPosition.z + ColliderSize; z++)
+                {
+                    if (x >= 0 && y >= 0 && z >= 0)
+                    {
+                        if (world[x, y, z].isMeshGen == false)
+                        {
+
+                            world[x, y, z].MeshColliderCall();
+
+                            world[x, y, z].isMeshGen = true;
+
+                        }
+                    }
+                }
+            }
+        }
     }
+
+
+
+
+
 
 
     public Vector3Int ChunkBlockToWorld(Vector3Int chunk, Vector3Int block)
